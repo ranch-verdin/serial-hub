0d7e614c6223f8fd945073b212365ec60499a214
The Hairy One <sasquatch@rickvenn.com>
2016-08-07 21:22:15 -0700

    port old alsaseq reader to new midi-stream-based

--- midi-glue/reader.lisp
+++ midi-glue/reader.lisp
@@ -18,72 +18,43 @@
 
 (defvar *reader-ochan* (make-nonblock-buf-channel))
 
-(defmacro if-gesture (&body body)
-  `((plist :EVENT-TYPE (guard event-type (or (equal event-type :snd_seq_event_noteoff)
-                                             (equal event-type :snd_seq_event_noteon)
-                                             (equal event-type
-                                                    :snd_seq_event_controller)
-                                             (equal event-type
-                                                    :snd_seq_event_pgmchange)
-                                             (equal event-type
-                                                    :snd_seq_event_chanpress)
-                                             (equal event-type
-                                                    :snd_seq_event_pitchbend)
-                                             (equal event-type
-                                                    :snd_seq_event_control14)
-                                             (equal event-type
-                                                    :snd_seq_event_nonregparam)
-                                             (equal event-type
-                                                    :snd_seq_event_regparam))))
-    ,@body))
-
-(defmacro if-clock (&body body)
-  `((plist :EVENT-TYPE (guard event-type (or (equal event-type
-                                                    :snd_seq_event_clock)
-                                             (equal event-type
-                                                    :snd_seq_event_start)
-                                             (equal event-type
-                                                    :snd_seq_event_stop)
-                                             (equal event-type
-                                                    :snd_seq_event_continue)
-                                             (equal event-type
-                                                    :snd_seq_event_songpos))))
-    ,@body))
-
-(defmacro macromatch (arg &body clauses)
-  `(match ,arg
-     ,@(mapcar #'macroexpand
-              clauses)))
-
-(defun midi-input (seq clock-ichan reader-ochan reader-map)
-  (let ((mess (recv seq)))
-    (macromatch mess
-      (if-gesture
-        (mapcar
-         (lambda (mapped-mess)
-           (! reader-ochan mapped-mess))
-         (funcall reader-map
-                  (list mess))))
-      (if-clock
-        (! clock-ichan mess)))))
+(defun midi-input (midi-stream clock-ichan reader-ochan
+		   ;; reader-map used to have a reader map here - but
+		   ;; I think better to burden application level with
+		   ;; mappings for their device
+		   )
+  (let ((message (read-midi-message midi-stream)))
+    (typecase message
+      (midi-performance-gesture (! reader-ochan message))
+      (midi-timing-message (! clock-ichan message)))))
+      ;; (midi-clock-gesture 
+      ;; (if-gesture
+      ;;   (mapcar
+      ;;    (lambda (mapped-mess)
+      ;;      (! reader-ochan mapped-mess))
+      ;;    (funcall reader-map
+      ;;             (list mess))))
+      ;; (if-clock
+      ;;   (! clock-ichan mess)))))
 
 (defvar *reader-thread* nil)
 
-(defun start-reader (clock-ichan &optional (reader-map #'identity))
+(defun start-reader (clock-ichan)
   (assert (null *reader-thread*))
   (setf *reader-thread*
         (bt:make-thread (lambda ()
-                          (sleep 1)
+                          (sleep 0.1)
                           (unwind-protect
                                (handler-case
-                                   (with-seq (seq :name "CL")
-                                     (open-port "in" seq :input)
+                                   (with-open-file (midi-stream "/dev/snd/midiC3D0"
+								:direction :io
+								:if-exists :overwrite
+								:element-type  '(unsigned-byte 8))
                                      (loop
                                         (restart-case
-                                            (midi-input seq
+                                            (midi-input midi-stream
                                                         clock-ichan
-                                                        *reader-ochan*
-                                                        reader-map)
+                                                        *reader-ochan*)
                                           (carry-on-reading ()))))
                                  (stop-thread ()))
                             (setf *reader-thread* nil)))
--- package.lisp
+++ package.lisp
@@ -6,15 +6,22 @@
 
 (defpackage #:midi-packetiser
   (:use #:cl #:optima #:serial-hub-utils)
-  (:export #:midi-message #:note-on-midi-message
+  (:export #:midi-message
+	   ;;Below are symbols related to performance gestures
+	   #:midi-performance-gesture #:note-on-midi-message
 	   #:note-off-midi-message #:key-pressure-midi-message
 	   #:control-change-midi-message #:channel-mode-midi-message
 	   #:program-change-midi-message #:channel-pressure-midi-message
-	   #:pitch-bend-midi-message #:sysex-message
+	   #:pitch-bend-midi-message
+
+	   #:sysex-message
 	   #:sysex-dump #:boomerang-sysex-message
-	   #:clock-message #:start-message
-	   #:continue-message #:stop-message
-	   #:song-position-pointer #:hi-nibble
+	   ;; Below are symbols related to midi clock
+	   #:midi-timing-message #:clock-message
+	   #:start-message #:continue-message
+	   #:stop-message #:song-position-pointer
+	   
+	   #:hi-nibble
 	   #:lo-nibble #:hi-bit
 	   #:pack-nibbles #:parse-packet
 	   #:read-midi-message))
